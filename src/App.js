b1b09e6b2870a2818d5a61e6268eaeacf58a7fa8
Patrick Lima <patrickhl94@hotmail.com>
2020-04-13 21:02:12 -0300

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [reposistories, setRepositories] = useState([]);
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    async function handleGetData() {
+
+      const response = await api.get('repositories');
+
+      setRepositories([...reposistories, ...response.data]);
+
+    }
+
+    handleGetData()
+
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+
+    const response = await api.post('repositories', {
+      title,
+      url,
+      techs
+    });
+
+    setRepositories([...reposistories, response.data])
+
+    console.log(response.data)
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    await api.delete(`repositories/${id}`)
+   
+    setRepositories(reposistories.filter(repo => repo.id !== id))
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {reposistories.map(repo => (<li key={repo.id}>
+          {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
-        </li>
+        </li>))
+        }
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button onClick={handleAddRepository}>Adicionar</button> <br />  <br />
+      <input placeholder="Title" type="text" onChange={event => setTitle(event.target.value)} /> <br />
+      <input placeholder="URL" type="text" onChange={event => setUrl(event.target.value)} /> <br />
+      <input placeholder="Techs" type="text" onChange={event => setTechs(event.target.value)} /> <br />
     </div>
   );
 }
